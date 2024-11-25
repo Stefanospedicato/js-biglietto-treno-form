c14bd997e180d7d4274b11612ae5d0a5578304a3
Stefanospedicato <stefano.spedicato11@gmail.com>
2024-11-25 15:31:34 +0100

    feat: add card element

--- script.js
+++ script.js
@@ -8,34 +8,15 @@ const generaPrezzo = document.getElementById('genera');
 const reset = document.getElementById('reset');
 const over = document.getElementById('over');
 const card = document.getElementById('card');
-
+const fullname = document.getElementById('fullname')
+const offerta = document.getElementById('offerta');
+const carrozza = document.getElementById('carrozza');
+const codice = document.getElementById('codice');
+const prezzoFinale = document.getElementById('prezzo');
 //Dati utili per calcolo del prezzo.
 const prezzoKm = 0.21;
 let price = 0;
 let sconto = 0;
-
-//Funzioni per generare prezzo e per il reset al cliccare dei bottoni.
-generaPrezzo.addEventListener('click', () =>{
-  const kms= parseFloat(km.value);
-  price = prezzoKm * kms;
-  
-  if (minorenne.checked) {sconto = 20 / 100
-  }
-  else if (over.checked) {sconto = 40 / 100
-  }
-  else{sconto = 0
-  }
-  let prezzoScontato = price * (1 - sconto);
-
-  card.classList.toggle('hide')
-
-})
-
-reset.addEventListener('click', ()=>{
-  km.value = '';
-  username.value='';
-  fasciaEta.value='Seleziona la tua età';
-})
 
 //Funzioni per numeri casuali per carrozza e numero biglietto.
 const random10 = num => Math.ceil(Math.random()*10)
@@ -43,3 +24,43 @@ const random10000 = num => Math.ceil(Math.random()*10000)
 let numero;
 console.log(random10(numero))
 console.log(random10000(numero))
+
+//Funzioni per generare prezzo e per il reset al cliccare dei bottoni.
+generaPrezzo.addEventListener('click', () =>{
+  const kms= parseFloat(km.value);
+  price = (prezzoKm * kms).toFixed(2);
+  
+  if (minorenne.checked) {sconto = 20 / 100;
+    offerta.innerHTML = 'Sconto Young'
+  }
+  else if (over.checked) {sconto = 40 / 100;
+    offerta.innerHTML = 'Sconto Over'
+  }
+  else{ offerta.innerHTML = 'Tariffa Standard'
+  }
+  let prezzoScontato = price * (1 - sconto);
+  
+  fullname.innerHTML = username.value.toUpperCase()
+  carrozza.innerHTML = random10();
+  codice.innerHTML = random10000();
+  prezzoFinale.innerHTML = price + ' ' + 'euro.';
+  card.classList.toggle('hide');
+})
+
+reset.addEventListener('click', ()=>{
+  km.value = '';
+  username.value='';
+  fasciaEta.value='Seleziona la tua età';
+  offerta.value ='';
+  fullname.value ='';
+  carrozza.value='';
+  codice.value='';
+  card.classList.toggle('hide')
+})
+
+
+
+
+
+
+
